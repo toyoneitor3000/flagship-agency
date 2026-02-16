9471c32c8440dfceeab2acf45e4b0adc51bf0dc5
Camo1 <toyoneitor3000@gmail.com>
2026-02-16 11:59:48 -0500

    Fix: Disable custom webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -26,13 +26,13 @@ const nextConfig = {
             },
         ],
     },
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.md$/,
-            type: 'asset/source',
-        });
-        return config;
-    },
+    // webpack: (config) => {
+    //     // config.module.rules.push({
+    //     //     test: /\.md$/,
+    //     //     type: 'asset/source',
+    //     // });
+    //     return config;
+    // },
 };
 
 export default nextConfig;
