4fc94b562abef561ab274b21a130e58dcb5cbaa8
Camo1 <toyoneitor3000@gmail.com>
2026-02-24 16:55:01 -0500

    feat: add Meta Pixel tracking (1430708191859507)

--- src/multiverse/pigmentostkts-web/src/app/layout.tsx
+++ src/multiverse/pigmentostkts-web/src/app/layout.tsx
@@ -8,6 +8,7 @@ import FloatingWhatsApp from "@/components/FloatingWhatsApp";
 import { auth } from "@/auth";
 
 const GA_MEASUREMENT_ID = "G-HREDYTNMLH";
+const META_PIXEL_ID = "1430708191859507";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const outfit = Outfit({
@@ -87,6 +88,25 @@ export default async function RootLayout({
           gtag('config', '${GA_MEASUREMENT_ID}');
         `}
       </Script>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${META_PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+        />
+      </noscript>
       <body className={`${inter.variable} ${outfit.variable} font-sans antialiased`}>
         <Providers session={session}>
           {children}
